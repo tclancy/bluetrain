fa8c23f64b84fd612f50d4201ffc40f8ee1d38f7
Tom Clancy <tclancy@gmail.com>
2012-08-20 12:25:49 -0400

    Trying to get this package to include everything.

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 from distutils.core import setup
 
 setup(
-	name = 'bluetrain',
-	packages = [
-		'bluetrain',
-		'bluetrain.fixtures',
-		'bluetrain.templates',
-		'bluetrain.templatetags',
-	],
-	version = '0.9.6.2',
-	description = 'Django CMS',
-	author='Tom Clancy',
-	author_email='tclancy@gmail.com',
-	url='https://bitbucket.org/tclancy/tkc_apps',
-	license = "LGPL",
-	classifiers = [
-		"Development Status :: 4 - Beta",
-		"Intended Audience :: Developers",
-		"License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
-		"Operating System :: OS Independent",
-		"Programming Language :: Python",
-		"Topic :: Software Development :: Libraries :: Python Modules",
-		"Framework :: Django"
-	],	
+    name='bluetrain',
+    packages=[
+            'bluetrain',
+            'bluetrain.fixtures',
+            'bluetrain.templates',
+            'bluetrain.templatetags',
+    ],
+    version='0.9.6.3',
+    description='Django CMS',
+    long_description=open('README.txt').read(),
+    author='Tom Clancy',
+    author_email='tclancy@gmail.com',
+    url='https://bitbucket.org/tclancy/tkc_apps',
+    license="LGPL",
+    classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "Framework :: Django"
+    ],
 )
